ec249a09bf85c7f0a1ffafd72eeebf71d5207353
Rediet Bekele <redu0587@gmail.com>
2024-09-11 12:31:57 +0000

    Done

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+sum= number1 + number2
+print(f"Sum of {number1} and {number2} is {sum}")
+
+Difference= number1-number2
+print(f"Difference of {number1} and {number2} is {Difference}")
+
+multiplication= number1*number2
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
